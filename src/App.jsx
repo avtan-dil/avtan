3f4526557499a09baa50d6026ab025f345baef11
HP <you@example.com>
2023-11-05 12:47:52 +0400

    qw

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Index from ".";
 
 import CountContextProvider from "./provider/CountContextProvider";
-console.log("asffasasfafs")
+
 function App() {
   return (
     <CountContextProvider>
