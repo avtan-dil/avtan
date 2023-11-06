ef750e7c03cda48cd56fcd6a30119c74e6e5506a
Your Name <youremail@example.com>
2023-11-06 09:44:54 +0400

    saxeli

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Index from ".";
 
 import CountContextProvider from "./provider/CountContextProvider";
-console.log("asgasg")
+
 function App() {
   return (
     <CountContextProvider>
