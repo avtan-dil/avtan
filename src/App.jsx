a30fe58f512eb707fe22255e2ccda2d0753a4a53
HP <you@example.com>
2023-11-06 09:31:34 +0400

    er

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Index from ".";
 
 import CountContextProvider from "./provider/CountContextProvider";
-console.log("asfsa")
+
 function App() {
   return (
     <CountContextProvider>
